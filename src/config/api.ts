7b4221d4b5b183a74449515228f32ee7c84c9f92
dwaldeck-light-it <daniela.waldeck@lightit.io>
2025-10-16 10:20:32 -0300

    feat: add api config

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,21 @@
-// TODO: Create your axios instance here
-export {};
+import axios from "axios";
+import { deepCamelKeys } from "string-ts";
+
+import { env } from "./env";
+
+const baseApiConfiguration = {
+  baseURL: env.VITE_API_URL,
+  headers: { "Content-Type": "application/json" },
+};
+
+const privateApi = axios.create(baseApiConfiguration);
+
+const publicApi = axios.create(baseApiConfiguration);
+
+publicApi.interceptors.response.use((response) => {
+  response.data = deepCamelKeys(response.data);
+
+  return response;
+});
+
+export { privateApi, publicApi };
