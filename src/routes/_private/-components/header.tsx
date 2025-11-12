135ec8289a14371f77e832c5b0b5007dd54e29c1
William Koelling <william.koelling@lightit.io>
2025-11-12 11:26:43 -0600

    feat: setup header skeleton

--- src/routes/_private/-components/header.tsx
+++ src/routes/_private/-components/header.tsx
@@ -1,7 +1,14 @@
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between bg-background-brand-default p-4 text-text-brand-on-brand">
-      <img alt="" className="h-10" src="./logo.svg" />
+    <header className="flex h-21 items-center justify-between bg-background-brand-default px-6 text-text-brand-on-brand">
+      <div className="flex items-center gap-3">
+        <img alt="" className="h-8 w-8 flex-shrink-0" src="./logo2.svg" />
+        <div>
+          <h1>Health Connect</h1>
+          <p>Find your healthcare provider</p>
+        </div>
+      </div>
+      <img alt="" className="h-8 w-8 flex-shrink-0" src="./logo2.svg" />
     </header>
   );
 };
