4aaa79074b428ff4e964ceb28e6fe26a29e485e6
Juan Catena <juan.catena@lightit.io>
2025-11-14 14:52:50 -0300

    refactor: change logo import

--- src/components/ui/icons/index.ts
+++ src/components/ui/icons/index.ts
@@ -1,1 +1,2 @@
+export * from "./brand-logo";
 export * from "./icons";
--- src/routes/_private/-components/header.tsx
+++ src/routes/_private/-components/header.tsx
@@ -1,11 +1,11 @@
-import { Avatar } from "@/components";
+import { Avatar, BrandLogo } from "@/components";
 
 export const Header = () => {
   return (
     <header className="bg-background-brand-default px-6 text-text-brand-on-brand md:px-12 lg:px-24">
       <div className="mx-auto flex h-21 max-w-6xl items-center justify-between">
         <div className="flex items-center gap-3">
-          <img alt="" className="h-8 w-8 flex-shrink-0" src="./logo2.svg" />
+          <BrandLogo />
           <div>
             <h2 className="">HealthConnect</h2>
             <span>Find your healthcare provider</span>
