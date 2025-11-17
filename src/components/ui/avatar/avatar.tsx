02aa6b16b42205ff9be0349599205fb92926d82f
william koelling <william.koelling@lightit.io>
2025-11-17 12:08:36 -0600

    feature/CU-86dycwwvk/Layout-Header (#6)
    * feat: setup header skeleton
    
    * feat: added avatar component
    
    * refactor: addessed pr comments
    
    * refactor: change logo import
    
    * style: updated header styling to match figma
    
    * refactor: changed export
    
    ---------
    
    Co-authored-by: Juan Catena <juan.catena@lightit.io>

---
+++ src/components/ui/avatar/avatar.tsx
@@ -0,0 +1,28 @@
+import { tv, type VariantProps } from "tailwind-variants";
+
+const avatarVariants = tv({
+  base: "flex items-center justify-center rounded-full",
+  variants: {
+    variant: {
+      primary: "bg-background-brand-default font-bold text-white",
+    },
+    size: {
+      sm: "h-6 w-6 text-xs",
+      default: "h-8 w-8 text-sm",
+      lg: "h-12 w-12 text-lg",
+    },
+  },
+
+  defaultVariants: {
+    variant: "primary",
+    size: "default",
+  },
+});
+
+export type AvatarProps = {
+  initials: string;
+} & VariantProps<typeof avatarVariants>;
+
+export const Avatar = ({ initials, size, variant }: AvatarProps) => {
+  return <div className={avatarVariants({ variant, size })}>{initials}</div>;
+};
--- src/routes/_private/-components/header.tsx
+++ src/routes/_private/-components/header.tsx
@@ -1,7 +1,22 @@
+import { Avatar, BrandLogo } from "@/components";
+
 export const Header = () => {
   return (
-    <header className="flex items-center justify-between bg-background-brand-default p-4 text-text-brand-on-brand">
-      <img alt="" className="h-10" src="./logo.svg" />
+    <header className="border-b-2 px-6 text-text-brand-on-brand md:px-12 lg:px-24">
+      <div className="mx-auto flex h-21 max-w-6xl items-center justify-between">
+        <div className="flex items-center gap-3">
+          <div className="rounded-lg bg-background-brand-default p-2">
+            <BrandLogo />
+          </div>
+          <div>
+            <h2 className="text-text-brand-on-brand-secondary">HealthConnect</h2>
+            <span className="font-extralight text-text-brand-on-brand-secondary">
+              Find your healthcare provider
+            </span>
+          </div>
+        </div>
+        <Avatar initials="WK" />
+      </div>
     </header>
   );
 };
