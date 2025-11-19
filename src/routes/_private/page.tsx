42e9c5f58caedc5010e4bb2c56af97c736ac0a37
william koelling <william.koelling@lightit.io>
2025-11-19 11:41:18 -0600

    [component] Text Stack (#7)
    * feat: added textstack comp
    
    * refactor: switched to compound component
    
    * refactor: switched to slots
    
    * refactor: replaced text in header
    
    * refactor: renamed textstack, used in page

--- src/routes/_private/page.tsx
+++ src/routes/_private/page.tsx
@@ -1,19 +1,19 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { Icons, Input, Subtext, Text, TextStack } from "@/components";
+import { TextStack } from "@/components";
 
 const HomePage = () => {
   return (
     <div className="flex flex-col gap-4">
-      <div className="mx-auto flex h-21 max-w-6xl flex-col">
-        <TextStack>
-          <Text className="text-lg text-text-brand-on-brand-secondary">Healthcare Providers</Text>
-          <Subtext className="text-base text-text-brand-on-brand-secondary">
+      <div className="mx-auto flex h-21 max-w-6xl flex-col justify-between">
+        <TextStack.Wrapper>
+          <TextStack.Text className="text-lg text-text-brand-on-brand-secondary">
+            Healthcare Providers
+          </TextStack.Text>
+          <TextStack.Subtext className="text-base text-text-brand-on-brand-secondary">
             Find and connect with healthcare professionals in your area
-          </Subtext>
-        </TextStack>
-
-        <Input left={<Icons.Search />} placeholder="Search healthcare providers" />
+          </TextStack.Subtext>
+        </TextStack.Wrapper>
       </div>
     </div>
   );
