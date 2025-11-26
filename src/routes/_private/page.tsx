25ce1dce64085848c476fa6dfe681a7b7e8a6ade
William Koelling <william.koelling@lightit.io>
2025-11-25 21:30:23 -0600

    refactor: simplified approach

--- src/routes/_private/page.tsx
+++ src/routes/_private/page.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { createFileRoute } from "@tanstack/react-router";
 
-import { Button, Filters, Icons, Input, TextStack } from "@/components";
+import { Button, FilterContainer, Icons, Input, TextStack } from "@/components";
 import { ITEMS } from "@/types/mock";
 
 const HomePage = () => {
@@ -11,20 +11,55 @@ const HomePage = () => {
   const [selectedClinicId, setSelectedClinicId] = useState(0);
 
   return (
-    <div className="flex flex-col gap-4">
-      <div className="mx-auto w-full max-w-6xl px-6 md:px-12 lg:px-24">
-        <div className="flex h-21 flex-col items-center justify-between">
-          <TextStack.Wrapper>
-            <TextStack.Text className="text-lg text-text-brand-on-brand-secondary">
-              Healthcare Providers
-            </TextStack.Text>
-            <TextStack.Subtext className="text-base text-text-brand-on-brand-secondary">
-              Find and connect with healthcare professionals in your area
-            </TextStack.Subtext>
-          </TextStack.Wrapper>
+    <div className="flex flex-col px-6 md:px-12 lg:px-24">
+      <div className="mx-auto w-full max-w-6xl flex-col">
+        <div className="flex flex-col gap-2">
+          <div className="flex h-21 w-full items-center justify-between">
+            <TextStack.Wrapper>
+              <TextStack.Text className="text-3xl text-text-brand-on-brand-secondary">
+                Healthcare Providers
+              </TextStack.Text>
+              <TextStack.Subtext className="text-base text-text-brand-on-brand-secondary">
+                Find and connect with healthcare professionals in your area
+              </TextStack.Subtext>
+            </TextStack.Wrapper>
+          </div>
+
+          <div className="flex w-full flex-col gap-4">
+            <Input left={<Icons.Search />} placeholder="Search healthcare providers" />
+
+            <div className="flex justify-between">
+              <div className="flex w-full flex-col gap-4 md:flex-row">
+                <FilterContainer
+                  items={ITEMS.genders}
+                  label="Gender"
+                  onSelect={setSelectedGenderId}
+                  selectedId={selectedGenderId}
+                />
+
+                <FilterContainer
+                  items={ITEMS.specialties}
+                  label="Specialty"
+                  onSelect={setSelectedSpecialtyId}
+                  selectedId={selectedSpecialtyId}
+                />
+
+                <FilterContainer
+                  items={ITEMS.clinics}
+                  label="Clinic"
+                  onSelect={setSelectedClinicId}
+                  selectedId={selectedClinicId}
+                />
+
+                <Button className="w-full py-3 md:w-auto" variant="secondary">
+                  <Icons.Heart /> Favorites
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <Filters.Root
+        {/* <Filters.Root
           filters={{
             gender: { selectedId: selectedGenderId, onSelect: setSelectedGenderId },
             specialty: { selectedId: selectedSpecialtyId, onSelect: setSelectedSpecialtyId },
@@ -47,7 +82,7 @@ const HomePage = () => {
               </Button>
             </Filters.FavoriteButton>
           </Filters.FilterRow>
-        </Filters.Root>
+        </Filters.Root> */}
       </div>
     </div>
   );
