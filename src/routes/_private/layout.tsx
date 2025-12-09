bd00d828c805c8aa6d6724383de188702d9659a4
William Koelling <william.koelling@lightit.io>
2025-12-09 10:57:18 -0600

    chore: removed comments

--- src/routes/_private/layout.tsx
+++ src/routes/_private/layout.tsx
@@ -1,8 +1,6 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 import { Header } from "./-components";
-// import { QueryClientProvider } from "@tanstack/react-query";
-// import { queryClient } from "@/config/query-client";
 
 const PrivateLayout = () => {
   return (
@@ -10,9 +8,7 @@ const PrivateLayout = () => {
       <Header />
 
       <main className="flex flex-col gap-4 p-4">
-        {/* <QueryClientProvider client={queryClient}> */}
         <Outlet />
-        {/* </QueryClientProvider> */}
       </main>
     </div>
   );
