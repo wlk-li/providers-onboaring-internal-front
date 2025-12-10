601b225be00680018ae683e8cb57b23f9c02689b
William Koelling <william.koelling@lightit.io>
2025-12-10 11:26:09 -0600

    feat: used RHF for state control

--- src/routes/_private/-components/filter-menu.tsx
+++ src/routes/_private/-components/filter-menu.tsx
@@ -1,42 +1,72 @@
-import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
 
 import { Button, DropdownFilter, Icons, Input } from "@/components";
 import { ITEMS } from "@/types/mock";
 
 export const FilterMenu = () => {
-  const [selectedGenderId, setSelectedGenderId] = useState(0);
-  const [selectedSpecialtyId, setSelectedSpecialtyId] = useState(0);
-  const [selectedClinicId, setSelectedClinicId] = useState(0);
+  const { control } = useForm({
+    defaultValues: {
+      gender: 0,
+      specialtyId: 0,
+      clinicId: 0,
+    },
+  });
 
   return (
     <div className="flex w-full flex-col gap-4">
       <Input left={<Icons.Search />} placeholder="Search healthcare providers" />
 
-      <div className="flex w-full flex-col gap-4 md:flex-row">
-        <DropdownFilter
-          items={ITEMS.genders}
-          label="Gender"
-          onSelect={setSelectedGenderId}
-          selectedId={selectedGenderId}
-        />
+      <div className="flex justify-between">
+        <div className="flex w-full flex-col gap-4 md:flex-row">
+          <Controller
+            control={control}
+            name="gender"
+            render={({ field }) => {
+              return (
+                <DropdownFilter
+                  items={ITEMS.genders}
+                  label="Gender"
+                  onSelect={field.onChange}
+                  selectedId={field.value}
+                />
+              );
+            }}
+          />
 
-        <DropdownFilter
-          items={ITEMS.specialties}
-          label="Specialty"
-          onSelect={setSelectedSpecialtyId}
-          selectedId={selectedSpecialtyId}
-        />
+          <Controller
+            control={control}
+            name="specialtyId"
+            render={({ field }) => {
+              return (
+                <DropdownFilter
+                  items={ITEMS.specialties}
+                  label="Specialty"
+                  onSelect={field.onChange}
+                  selectedId={field.value}
+                />
+              );
+            }}
+          />
 
-        <DropdownFilter
-          items={ITEMS.clinics}
-          label="Clinic"
-          onSelect={setSelectedClinicId}
-          selectedId={selectedClinicId}
-        />
+          <Controller
+            control={control}
+            name="clinicId"
+            render={({ field }) => {
+              return (
+                <DropdownFilter
+                  items={ITEMS.clinics}
+                  label="Clinic"
+                  onSelect={field.onChange}
+                  selectedId={field.value}
+                />
+              );
+            }}
+          />
 
-        <Button className="w-full py-3 md:max-w-30" variant="secondary">
-          <Icons.Heart /> Favorites
-        </Button>
+          <Button className="w-full py-3 md:max-w-30" variant="secondary">
+            <Icons.Heart /> Favorites
+          </Button>
+        </div>
       </div>
     </div>
   );
